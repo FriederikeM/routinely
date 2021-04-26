33c041bd885ca91c538d752dea7ff0ce16812b66
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-26 14:27:51 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello from my routinely capstone</div>
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <h1>Hello from the home</h1>
+            </Route>
+            <Route exact path="/weekly-routine">
+              <h1>Hello from the weekly routine</h1>
+            </Route>
+            <Route exact path="/products">
+              <h1>Hello from the product list</h1>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
