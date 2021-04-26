e0dce59a7665ed580d621f797fc875f3b8b15b29
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-26 22:55:52 +0200

    Add animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,21 @@ import bottleTwo from "../images/bottle-two.png";
 export default function Home() {
   return (
     <div className="home">
-      <h1 className="headline">routinely</h1>
-      <a href="/products" className="product-link">
-        Check out all products
-      </a>
-      <img src={bottleOne} alt="bottle" className="bottle-one" />
-
-      <a href="/weekly-routine" className="routine-link">
-        View your weekly routine
-      </a>
-      <img src={bottleTwo} alt="bottle" className="bottle-two" />
+      <div className="headline-wrapper">
+        <h1 className="headline">routinely</h1>
+      </div>
+      <div className="bottle-wrapper-one">
+        <a href="/products" className="product-link">
+          Check out all products
+        </a>
+        <img src={bottleOne} alt="bottle" className="bottle-one" />
+      </div>
+      <div className="bottle-wrapper-two">
+        <a href="/weekly-routine" className="routine-link">
+          View your weekly routine
+        </a>
+        <img src={bottleTwo} alt="bottle" className="bottle-two" />
+      </div>
     </div>
   );
 }
