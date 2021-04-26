a4c1442c0bd0d32d3adacb0301f08b56d11d4c00
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-26 15:43:40 +0200

    Remove main tag around Switch

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <h1>Hello from the home</h1>
-            </Route>
-            <Route exact path="/weekly-routine">
-              <h1>Hello from the weekly routine</h1>
-            </Route>
-            <Route exact path="/products">
-              <h1>Hello from the product list</h1>
-            </Route>
-          </Switch>
-        </main>
+        <Switch>
+          <Route exact path="/">
+            <h1>Hello from the home</h1>
+          </Route>
+          <Route exact path="/weekly-routine">
+            <h1>Hello from the weekly routine</h1>
+          </Route>
+          <Route exact path="/products">
+            <h1>Hello from the product list</h1>
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
