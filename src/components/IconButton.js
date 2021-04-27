52fa2f0cb2b4918e225ce8acaa64ea4dd04e14cc
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-27 14:03:57 +0200

    Set up ButtonIcon Component

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,5 +1,13 @@
 import "./IconButton.css";
+import { ImCalendar } from "react-icons/im";
+import { NavLink } from "react-router-dom";
 
 export default function IconButton() {
-  return <div className="IconButton">IB</div>;
+  return (
+    <div className="IconButton">
+      <NavLink to="/weekly-routine" className="weekly-routine-link">
+        <ImCalendar className="calendar" />
+      </NavLink>
+    </div>
+  );
 }
