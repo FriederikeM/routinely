7aa7f521f168c30b7183976031f08680391235ec
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-27 11:53:41 +0200

    Set up name search form

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,25 @@
 import "./Filter.css";
 
 export default function Filter() {
-  return <div className="Filter">Hello from the Filter</div>;
+  return (
+    <div className="Filter">
+      <form className="filter-form">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Search for a product"
+          aria-label="name search input"
+          className="name-search-bar"
+        />
+        <button
+          type="submit"
+          aria-label="name search submit"
+          className="name-search-button"
+        >
+          Go
+        </button>
+      </form>
+    </div>
+  );
 }
