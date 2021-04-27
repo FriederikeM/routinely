f78a701e8418a337ec9d9f57c34f5bbab69c7602
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-27 10:18:17 +0200

    Change anchors to NavLinks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import "./Home.css";
 import bottleOne from "../images/bottle-one.png";
 import bottleTwo from "../images/bottle-two.png";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -9,15 +10,15 @@ export default function Home() {
         <h1 className="headline">routinely</h1>
       </div>
       <div className="bottle-wrapper-one">
-        <a href="/products" className="product-link">
+        <NavLink to="/products" className="product-link">
           Check out all products
-        </a>
+        </NavLink>
         <img src={bottleOne} alt="bottle" className="bottle-one" />
       </div>
       <div className="bottle-wrapper-two">
-        <a href="/weekly-routine" className="routine-link">
+        <NavLink to="/weekly-routine" className="routine-link">
           View your weekly routine
-        </a>
+        </NavLink>
         <img src={bottleTwo} alt="bottle" className="bottle-two" />
       </div>
     </div>
