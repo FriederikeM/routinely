a2b3a0437bf5f50a4bc00956e714d93a75f0846e
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-29 22:30:37 +0200

    Set up week day cards statically

---
+++ src/components/WeekDayCard.js
@@ -0,0 +1,48 @@
+import "./WeekDayCard.css";
+import { HiSun } from "react-icons/hi";
+import { FaMoon } from "react-icons/fa";
+
+export default function WeekDayCard() {
+  return (
+    <div className="WeekDayCard">
+      <div className="weekday">
+        <h4 className="weekday-headline">monday</h4>
+        <div className="weekday-box">
+          <div className="weekday-box-header">
+            <HiSun className="sun" />
+            <FaMoon />
+          </div>
+          <section className="morning">
+            <ul className="product-name-list">
+              <li className="product-name-left">Niacinamide 10% + Zinc 1%</li>
+              <li className="product-name-left">Azelaic Acid Suspension 10%</li>
+              <li className="product-name-left">Hyaluronic Acid 2% + B5</li>
+              <li className="product-name-left">
+                Natural Moisturizing Factors + HA
+              </li>
+              <li className="product-name-left">
+                Mineral UV Filters SPF 30 with Antioxidants
+              </li>
+            </ul>
+          </section>
+          <section className="evening">
+            <ul className="product-name-list">
+              <li className="product-name-right">Squalane Cleanser</li>
+              <li className="product-name-right">Argireline Solution 10%</li>
+              <li className="product-name-right">Hyaluronic Acid 2% + B5</li>
+              <li className="product-name-right">
+                Natural Moisturizing Factors + HA
+              </li>
+              <li className="product-name-right">
+                100% Organic Cold-Pressed Rose Hip Seed Oil
+              </li>
+            </ul>
+          </section>
+          <div className="see-details-button-wrapper">
+            <button className="see-details-button">See Details</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
