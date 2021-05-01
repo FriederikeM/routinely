1dfdb672d195ab9fdecb64a93916df708d5b0b46
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-01 16:52:48 +0200

    Fetch products from json file

--- src/components/WeeklyRoutine.js
+++ src/components/WeeklyRoutine.js
@@ -5,64 +5,18 @@ import { getDataFromLocalStorage } from "../utility/localStorage";
 import { useEffect } from "react";
 
 export default function WeeklyRoutine() {
-  const ids = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-    24,
-    25,
-    26,
-    27,
-    28,
-    29,
-    30,
-    31,
-    32,
-    33,
-    34,
-    35,
-    36,
-    37,
-    38,
-    39,
-    40,
-    41,
-    42,
-    43,
-    44,
-    45,
-    46,
-    47,
-    48,
-    49,
-  ];
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    ids.map((id) => {
-      const productData = getDataFromLocalStorage(id);
-      console.log(productData);
-      return productData;
+    const fetchProducts = () => {
+      return fetch("products.json")
+        .then((response) => response.json())
+        .then((productData) => {
+          setProducts(productData);
     });
+    };
+    fetchProducts();
+  }, []);
   }, []);
 
   return (
