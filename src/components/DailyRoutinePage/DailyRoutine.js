e6ffaf04c3a794599c7b7d32845b757b33b1a6ec
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-03 10:47:50 +0200

    Return page setup with header, main and product card

--- src/components/DailyRoutinePage/DailyRoutine.js
+++ src/components/DailyRoutinePage/DailyRoutine.js
@@ -1,5 +1,32 @@
 import "./DailyRoutine.css";
+import AddedProductCard from "./AddedProductCard.js";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function DailyRoutine() {
-  return <div className="DailyRoutine">Hello from the daily routine</div>;
+  return (
+    <div className="DailyRoutine">
+      <header className="daily-header">
+        <h1 className="daily-headline">monday</h1>
+        <button className="history-back">
+          <FaArrowLeft />
+        </button>
+      </header>
+      <main className="daily-main">
+        <section className="morning-products-display">
+          <h2 className="daytime-headline">morning</h2>
+          <AddedProductCard />
+          <AddedProductCard />
+          <AddedProductCard />
+          <AddedProductCard />
+        </section>
+        <section className="evening-products-display">
+          <h2 className="daytime-headline">evening</h2>
+          <AddedProductCard />
+          <AddedProductCard />
+          <AddedProductCard />
+          <AddedProductCard />
+        </section>
+      </main>
+    </div>
+  );
 }
