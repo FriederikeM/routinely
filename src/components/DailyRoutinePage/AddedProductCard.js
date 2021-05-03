aab143abd3de6ae64f505cd6d59fc89b7aea3ce4
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-03 12:01:06 +0200

    Add className

--- src/components/DailyRoutinePage/AddedProductCard.js
+++ src/components/DailyRoutinePage/AddedProductCard.js
@@ -5,14 +5,20 @@ import bottleimg from "../../images/niacinamide-bottle.png";
 export default function AddedProductCard() {
   return (
     <div className="AddedProductCard">
-      <h5>Niacinamide 10% + Zinc 1%</h5>
-      <p>opened: february</p>
-      <p>lasts: 12 months</p>
-      <p>
-        running low? <TiShoppingCart />{" "}
+      <h5 className="added-product-name">Niacinamide 10% + Zinc 1%</h5>
+      <div className="time-details">
+        <p>opened: february</p>
+        <p>lasts: 12 months</p>
+      </div>
+      <p className="restock">
+        running low? <TiShoppingCart className="shopping-cart-icon" />{" "}
       </p>
       <button className="edit-button">edit</button>
-      <img src={bottleimg} alt="product bottle" width="100px"></img>
+      <img
+        src={bottleimg}
+        alt="product bottle"
+        className="added-bottle-image"
+      ></img>
     </div>
   );
 }
