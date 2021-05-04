127339f23eedd72ab9b49ef5a05969c717644bd0
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-04 22:07:09 +0200

    Change method to find instead of filter

--- src/utility/getProductById.js
+++ src/utility/getProductById.js
@@ -1,6 +1,6 @@
 export default function getProductById(id, products) {
-  const foundProduct = products.filter((product) => {
+  const foundProduct = products.find((product) => {
     return product.id === id;
   });
-  return foundProduct[0];
+  return foundProduct;
 }
