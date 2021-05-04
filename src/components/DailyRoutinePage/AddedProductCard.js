1b5cc37cbf46bbc060d8b06baf82f8cab28f46a3
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-04 12:55:21 +0200

    Render products depending on weekday

--- src/components/DailyRoutinePage/AddedProductCard.js
+++ src/components/DailyRoutinePage/AddedProductCard.js
@@ -2,23 +2,59 @@ import "./AddedProductCard.css";
 import { TiShoppingCart } from "react-icons/ti";
 import bottleimg from "../../images/niacinamide-bottle.png";
 
-export default function AddedProductCard() {
+import getProductsById from "../../utility/getProductsById";
+
+export default function AddedProductCard({ info, products, name, time }) {
+  let i;
+  if (name === "Monday") {
+    i = 0;
+  } else if (name === "Tuesday") {
+    i = 1;
+  } else if (name === "Wednesday") {
+    i = 2;
+  } else if (name === "Thursday") {
+    i = 3;
+  } else if (name === "Friday") {
+    i = 4;
+  } else if (name === "Saturday") {
+    i = 5;
+  } else if (name === "Sunday") {
+    i = 6;
+  }
+
+  const weekdayTime = info.filter((product) => {
+    return product.days[i][time];
+  });
+
+  const openingDate = info.filter((product) => {
+    return product.date;
+  });
+
+  console.log(products);
+
   return (
-    <div className="AddedProductCard">
-      <h5 className="added-product-name">Niacinamide 10% + Zinc 1%</h5>
-      <div className="time-details">
-        <p>opened: february</p>
-        <p>lasts: 12 months</p>
-      </div>
-      <p className="restock">
-        running low? <TiShoppingCart className="shopping-cart-icon" />{" "}
-      </p>
-      <button className="edit-button">edit</button>
-      <img
-        src={bottleimg}
-        alt="product bottle"
-        className="added-bottle-image"
-      ></img>
-    </div>
+    products.length > 0 &&
+    weekdayTime.map((time) => {
+      return (
+        <div className="AddedProductCard">
+          <h5 className="added-product-name">
+            {getProductsById(time.id, products).name}
+          </h5>
+          <div className="time-details">
+            {openingDate !== "" && <p>opened: {openingDate}</p>}
+            <p>lasts: {getProductsById(time.id, products).expirationPeriod}</p>
+          </div>
+          <p className="restock">
+            running low? <TiShoppingCart className="shopping-cart-icon" />{" "}
+          </p>
+          <button className="edit-button">edit</button>
+          <img
+            src={bottleimg}
+            alt="product bottle"
+            className="added-bottle-image"
+          ></img>
+        </div>
+      );
+    })
   );
 }
--- src/components/DailyRoutinePage/DailyRoutine.js
+++ src/components/DailyRoutinePage/DailyRoutine.js
@@ -2,16 +2,57 @@ import "./DailyRoutine.css";
 import AddedProductCard from "./AddedProductCard.js";
 import { FaArrowLeft } from "react-icons/fa";
 import { useParams, useHistory } from "react-router-dom";
+import { getDataFromLocalStorage } from "../../utility/localStorage.js";
+import { useEffect, useState } from "react";
 
 export default function DailyRoutine() {
   const { weekday } = useParams();
   const history = useHistory();
+  const [allItems, setAllItems] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = () => {
+      return fetch("/products.json")
+        .then((response) => response.json())
+        .then((productData) => {
+          setProducts(productData);
+        });
+    };
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const routine = getDataFromLocalStorage();
+    setAllItems(routine);
+  }, [weekday]);
+
+  let i;
+  if (weekday === "Monday") {
+    i = 0;
+  } else if (weekday === "Tuesday") {
+    i = 1;
+  } else if (weekday === "Wednesday") {
+    i = 2;
+  } else if (weekday === "Thursday") {
+    i = 3;
+  } else if (weekday === "Friday") {
+    i = 4;
+  } else if (weekday === "Saturday") {
+    i = 5;
+  } else if (weekday === "Sunday") {
+    i = 6;
+  }
+
+  const weekdayArray = allItems.filter((item) => {
+    return item.days[i].isChecked;
+  });
 
   return (
     <div className="DailyRoutine" key={weekday}>
       <header className="daily-header">
         <div className="daily-headline-wrapper">
-          <h1 className="daily-headline">monday</h1>
+          <h1 className="daily-headline">{weekday}</h1>
         </div>
         <button className="history-back" onClick={() => history.goBack()}>
           <FaArrowLeft />
@@ -21,19 +62,23 @@ export default function DailyRoutine() {
         <section className="morning-products-display">
           <h2 className="daytime-headline">morning</h2>
           <article className="morning-products-list">
-            <AddedProductCard />
-            <AddedProductCard />
-            <AddedProductCard />
-            <AddedProductCard />
+            <AddedProductCard
+              products={products}
+              time="morning"
+              info={weekdayArray}
+              name={weekday}
+            />
           </article>
         </section>
         <section className="evening-products-display">
           <h2 className="daytime-headline">evening</h2>
           <article className="evening-products-list">
-            <AddedProductCard />
-            <AddedProductCard />
-            <AddedProductCard />
-            <AddedProductCard />
+            <AddedProductCard
+              products={products}
+              time="evening"
+              info={weekdayArray}
+              name={weekday}
+            />
           </article>
         </section>
       </main>
