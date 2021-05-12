63ea652de7972d3c2457c6dfbfe36f01e3d1a043
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-13 00:26:48 +0200

    Add slash to resolve Syntax error

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -4,7 +4,7 @@ export default function useProducts() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchProducts = () => {
-      return fetch("products.json")
+      return fetch("/products.json")
         .then((response) => response.json())
         .then((productData) => {
           setProducts(productData);
