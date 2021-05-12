017170c7c4e19ca3ae865d5f130359e986449ca8
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-12 10:43:46 +0200

    Introduce custom hook to stop item from displacing when being grabbed

--- src/components/WeeklyRoutinePage/WeekDayCard.js
+++ src/components/WeeklyRoutinePage/WeekDayCard.js
@@ -92,6 +92,36 @@ export default function WeekDayCard({ name, data, products }) {
     setEveningProductNames(items);
   }
 
+  //custom hook to avoid box from displacing itself when grabbed
+  const useDraggableInPortal = () => {
+    const self = useRef({}).current;
+
+    useEffect(() => {
+      const div = document.createElement("div");
+      div.style.position = "absolute";
+      div.style.pointerEvents = "none";
+      div.style.top = "0";
+      div.style.width = "100%";
+      div.style.height = "100%";
+      div.style.listStyle = "none";
+      div.style.fontSize = "0.8rem";
+      self.elt = div;
+      document.body.appendChild(div);
+      return () => {
+        document.body.removeChild(div);
+      };
+    }, [self]);
+
+    return (render) => (provided, ...args) => {
+      const element = render(provided, ...args);
+      if (provided.draggableProps.style.position === "fixed") {
+        return createPortal(element, self.elt);
+      }
+      return element;
+    };
+  };
+
+  const renderDraggable = useDraggableInPortal();
 
   return (
     <div className="WeekDayCard">
