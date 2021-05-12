06a4c2d8402d0fbd2ee9fb244f73095506431c72
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-12 20:58:14 +0200

    Create custom hook to fetch products from json file

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export default function useProducts() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = () => {
+      return fetch("products.json")
+        .then((response) => response.json())
+        .then((productData) => {
+          setProducts(productData);
+        });
+    };
+    fetchProducts();
+  }, []);
+  return products;
+}
