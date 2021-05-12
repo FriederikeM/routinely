7b34a25672696f3744c97f5c1acb6ea59c201eb4
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-12 23:24:12 +0200

    Outsource function to get products checked on that morning and evening

--- src/components/WeeklyRoutinePage/WeekDayCard.js
+++ src/components/WeeklyRoutinePage/WeekDayCard.js
@@ -3,69 +3,14 @@ import { HiSun } from "react-icons/hi";
 import { FaMoon } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import getProductById from "../../utility/getProductById.js";
+import {
+  getProductsCheckedOnThisMorning,
+  getProductsCheckedOnThisEvening,
+} from "../../utility/getCheckedProducts";
 
 export default function WeekDayCard({ name, data, products }) {
-  let mornings;
-  if (name === "Monday") {
-    mornings = data.filter((monday) => {
-      return monday.days[0].morning;
-    });
-  } else if (name === "Tuesday") {
-    mornings = data.filter((tuesday) => {
-      return tuesday.days[1].morning;
-    });
-  } else if (name === "Wednesday") {
-    mornings = data.filter((wednesday) => {
-      return wednesday.days[2].morning;
-    });
-  } else if (name === "Thursday") {
-    mornings = data.filter((thursday) => {
-      return thursday.days[3].morning;
-    });
-  } else if (name === "Friday") {
-    mornings = data.filter((friday) => {
-      return friday.days[4].morning;
-    });
-  } else if (name === "Saturday") {
-    mornings = data.filter((saturday) => {
-      return saturday.days[5].morning;
-    });
-  } else if (name === "Sunday") {
-    mornings = data.filter((sunday) => {
-      return sunday.days[6].morning;
-    });
-  }
-
-  let evenings;
-  if (name === "Monday") {
-    evenings = data.filter((monday) => {
-      return monday.days[0].evening;
-    });
-  } else if (name === "Tuesday") {
-    evenings = data.filter((tuesday) => {
-      return tuesday.days[1].evening;
-    });
-  } else if (name === "Wednesday") {
-    evenings = data.filter((wednesday) => {
-      return wednesday.days[2].evening;
-    });
-  } else if (name === "Thursday") {
-    evenings = data.filter((thursday) => {
-      return thursday.days[3].evening;
-    });
-  } else if (name === "Friday") {
-    evenings = data.filter((friday) => {
-      return friday.days[4].evening;
-    });
-  } else if (name === "Saturday") {
-    evenings = data.filter((saturday) => {
-      return saturday.days[5].evening;
-    });
-  } else if (name === "Sunday") {
-    evenings = data.filter((sunday) => {
-      return sunday.days[6].evening;
-    });
-  }
+  const mornings = getProductsCheckedOnThisMorning(name, data);
+  const evenings = getProductsCheckedOnThisEvening(name, data);
 
   return (
     <div className="WeekDayCard">
--- src/utility/getCheckedProducts.js
+++ src/utility/getCheckedProducts.js
@@ -8,3 +8,19 @@ export function getProductsCheckedOnThisDay(weekday, allItems) {
   });
   return checkedProducts;
 }
+
+export function getProductsCheckedOnThisMorning(weekday, data) {
+  const weekdayIndex = getIndexForWeekday(weekday);
+  const mornings = data.filter((item) => {
+    return item.days[weekdayIndex].morning;
+  });
+  return mornings;
+}
+
+export function getProductsCheckedOnThisEvening(weekday, data) {
+  const weekdayIndex = getIndexForWeekday(weekday);
+  const evenings = data.filter((item) => {
+    return item.days[weekdayIndex].evening;
+  });
+  return evenings;
+}
