2c644076932a90664e0356cc41e7ecd26530b36c
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-16 15:12:14 +0200

    Find clicked product by id and destructure its propertiy names

--- src/components/SingleProductPage/SingleProduct.js
+++ src/components/SingleProductPage/SingleProduct.js
@@ -1,8 +1,12 @@
 import { useParams, useHistory } from "react-router";
 import useProducts from "../../hooks/useProducts";
+import getProductById from "../../utility/getProductById";
 
 export default function SingleProduct() {
   const { singleProductId } = useParams();
   const products = useProducts();
   const history = useHistory();
+    const productInfo = getProductById(Number(singleProductId), products);
+    const { name, image, ingredients, time, refrigeration } = productInfo;
+
 }
