60358fd444f9272609d6f0552ed79ceb98b0831a
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-21 23:08:09 +0200

    Document code

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
 
+/**
+ * fetches products array from JSON file and updates products useState with the new array
+ * @type {function}
+ * @returns {array<object>}
+ */
+
 export default function useProducts() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
--- src/hooks/useRoutine.js
+++ src/hooks/useRoutine.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import { getDataFromLocalStorage } from "../utility/localStorage";
 
+/**
+ * gets products array from LocalStorage and updates allRoutineItems useState with array
+ * @type {function}
+ * @returns {array<object>}
+ */
+
 export default function useRoutine() {
   const [allRoutineItems, setAllRoutineItems] = useState([]);
   useEffect(() => {
