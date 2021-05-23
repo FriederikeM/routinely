96f866f0834c5cc6235b5f885fd7f1c4da182888
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-23 22:53:46 +0200

    Write and run test to filter products by name

---
+++ cypress/integration/tests/name_filter_products.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Navigate to products page and filter products by name", () => {
+  it("finds the link to the product page, clicks it and gets directed there", () => {
+    cy.visit("http://localhost:4000");
+    cy.get(".product-link").click();
+    cy.url().should("include", "/products");
+  });
+
+  it("Types in name of a product and shows the products including the typed in name", () => {
+    cy.get(".name-search-bar").focus().type("Niacinamide");
+    cy.get(".name-search-bar").should("have.value", "Niacinamide");
+    cy.get(".ProductCard").should("have.length", 2);
+  });
+});
