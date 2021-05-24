06130a028b5a8601b9dfd01b9aed1cae7aa82491
Friederike Moeser <friederike.moeser@gmail.com>
2021-05-24 22:32:02 +0200

    Add function to check if a day has been unchecked even though morning or evening were still checked

--- src/utility/isThisTimeChecked.js
+++ src/utility/isThisTimeChecked.js
@@ -9,3 +9,23 @@ export default function isThisTimeChecked(routineData, name, timeOfDay, id) {
     return false;
   }
 }
+
+export function isDayUncheckedButMorningChecked(weekRoutine) {
+  const morningChecked = weekRoutine.days
+    .map((day) => {
+      return day.isChecked === false && day.morning === true;
+    })
+    .some((day) => day === true);
+  console.log(morningChecked);
+  return morningChecked;
+}
+
+export function isDayUncheckedButEveningChecked(weekRoutine) {
+  const eveningChecked = weekRoutine.days
+    .map((day) => {
+      return day.isChecked === false && day.evening === true;
+    })
+    .some((day) => day === true);
+  console.log(eveningChecked);
+  return eveningChecked;
+}
